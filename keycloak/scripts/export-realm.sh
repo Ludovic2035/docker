6073b70857c1b846c8e4b7821e7feadeb48307c2
Sebastian Drost <s.drost@52north.org>
2022-11-14 09:44:47 +0100

    Improve export script for running without error codes

--- keycloak/scripts/export-realm.sh
+++ keycloak/scripts/export-realm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it keycloak bash -c "/opt/keycloak/bin/kc.sh export --dir /tmp/export --realm kommonitor --users realm_file"
+docker exec -it keycloak bash -c "QUARKUS_HTTP_HOST_ENABLED=false /opt/keycloak/bin/kc.sh export --dir /tmp/export --realm kommonitor --users realm_file"
