ab7cbb1b802afadc3245a4837f49c770308af466
Sebastian Drost <s.drost@52north.org>
2023-12-18 11:38:49 +0100

    Add config script for template files

---
+++ config/config.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ "$KOMMONITOR_NAME" == "kommonitor-client-config" ]
+then
+    apk add gettext
+    if [ -f /code/configStorage/webClientAppConfig.js ];
+    then
+        echo "Web Client App configuration already set."
+    else
+        echo "Set Web Client App configuration."
+        envsubst < /code/templates/webClientAppConfig.js.template > /code/configStorage/webClientAppConfig.js
+    fi
+    if [ -f /code/configStorage/webClientKeycloakConfig.json ];
+    then
+        echo "Web Client Keycloak Config already set."
+    else
+        echo "Set Web Client Keycloak Config."
+        envsubst < /code/templates/webClientKeycloakConfig.json.template > /code/configStorage/webClientKeycloakConfig.json
+    fi
+    npm start
+fi
+
+if [ "$KOMMONITOR_NAME" == "kommonitor-web-client" ]
+then
+    apk add gettext
+    if [ -f /usr/share/nginx/html/config/config-storage-server.json ];
+    then
+        echo "Client Config Server configuration already set."
+    else
+        echo "Set Client Config Server configuration."
+        envsubst < /code/templates/config-storage-server.json.template > /usr/share/nginx/html/config/config-storage-server.json
+    fi
+    nginx -g 'daemon off;'
+fi
